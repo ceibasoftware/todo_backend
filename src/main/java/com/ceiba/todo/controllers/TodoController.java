911f7df2464158407c3b7f0d5b14eefa54531001
German Arroyave Garcia - Ceiba Software <german.arroyave@ceiba.com.co>
2019-03-18 09:11:44 -0500

    [Initial commit] configuration of the database and impementation of the connection to it from the backend

---
+++ src/main/java/com/ceiba/todo/controllers/TodoController.java
@@ -0,0 +1,10 @@
+package com.ceiba.todo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TodoController {
+
+}
