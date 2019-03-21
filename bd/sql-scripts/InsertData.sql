dcf063c051aee4d311f8c9bfad934ac4750eaba3
German Arroyave Garcia - Ceiba Software <german.arroyave@ceiba.com.co>
2019-03-21 09:36:26 -0500

    [ISSUE] database scripts migration flyway

--- bd/sql-scripts/InsertData.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users (name, user_name) VALUES ('German A', 'german.arroyave');
-INSERT INTO lists (user_id, name, description) VALUES (1, 'Others', 'General activities');
-INSERT INTO tasks (title, done, list_id) VALUES ('Hacer backend', false, 1);
-INSERT INTO tasks (title, done, list_id) VALUES ('Hacer frontend', false, 1);
-INSERT INTO tasks (title, done, list_id) VALUES ('Hacer docker Mysql', true, 1);
-
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'; 
---
+++ src/main/resources/db/migration/V1__Init.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS users (
+	user_id INT AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	user_name VARCHAR(50) NOT NULL,
+	PRIMARY KEY (user_id)
+) ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS lists (
+	list_id INT AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	description VARCHAR(50),
+	PRIMARY KEY (list_id),
+	FOREIGN KEY (user_id) REFERENCES users(user_id)
+) ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS tasks (
+	task_id INT AUTO_INCREMENT,
+	title VARCHAR(255) NOT NULL,
+	done BOOLEAN NOT NULL,
+	list_id INT NOT NULL,
+	PRIMARY KEY (task_id),
+	FOREIGN KEY (list_id) REFERENCES lists(list_id)
+) ENGINE=INNODB;
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
