b58952618786c2f26e1fdec1bb4cb29bd9a8877d
German Arroyave Garcia - Ceiba Software <german.arroyave@ceiba.com.co>
2019-03-21 08:43:20 -0500

    [ISSUE] gradle wrapper

--- src/main/java/com/ceiba/todo/TodoListApp.java
+++ src/main/java/com/ceiba/todo/TodoListApp.java
@@ -1,15 +1,13 @@
 package com.ceiba.todo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
-import org.springframework.test.context.ContextConfiguration;
-
-import com.ceiba.todo.config.JdbcConfig;
 
 @SpringBootApplication
 @EnableJdbcRepositories
-@ContextConfiguration(classes = JdbcConfig.class)
+@EnableAutoConfiguration
 public class TodoListApp {
 
 	public static void main(String[] args) {
